3d1d1f00c348f4ea46d3ea3b7d600a52cc725a5c
afrodekov1ch <omurcomp@gmail.com>
2024-01-10 23:26:27 +0600

    посидел пипирку потёр
    и пошло поехало

--- RAZDEVALKI OT BEKI/Assets/Scripts/ClickMoney.cs
+++ RAZDEVALKI OT BEKI/Assets/Scripts/ClickMoney.cs
@@ -5,6 +5,12 @@ using UnityEngine.UI;
 
 public class ClickMoney : MonoBehaviour
 {
+    public Text[] tyanScoreTxt;
+    public int[] tyanScore;
+    public Text autoText;
+    public int auto;
+    public Text powerText;
+    public int power;
     public int score;
     public Text clicText;
     void Start()
@@ -16,11 +22,21 @@ public class ClickMoney : MonoBehaviour
 
     void Update()
     {
-        clicText.text = score.ToString();
+        if(score <= tyanScore[1])
+        {
+            tyanScore[0] = tyanScore[1] - score;
+        }
+        else{
+            tyanScore[0] = 0;
+        }
+        tyanScoreTxt[0].text = "Монет: " + tyanScore[0] + "";
+        autoText.text = "+" + auto + " автоклик";
+        powerText.text = "+" + power + " за клик";
+        clicText.text = "Монет: " + score;
     }
     public void clicerScore()
     {
-        score++;
+        score += power;
         PlayerPrefs.SetInt("Score+", score);
     }
 }
